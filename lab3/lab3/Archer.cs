7ddba1fadbf1cfea21faf3b6c72135bb00778746
Dmewtriy <dankin.dm@mail.ru>
2024-11-06 11:27:57 +0700

    Create MobClass Classes

---
+++ lab3/lab3/Archer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Archer : Mob
+    {
+        public void Attack(Mage enemy)
+        {
+            base.Attack(enemy);
+            enemy.HP -= 2;
+        }
+
+        public void Attack(Tank enemy)
+        {
+            base.Attack(enemy);
+            enemy.HP += 2;
+        }
+    }
+}
---
+++ lab3/lab3/Melee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Melee : Mob
+    {
+        public void Attack(Archer enemy)
+        {
+            base.Attack(enemy);
+            enemy.HP -= 2;
+        }
+
+        public void Attack(Tank enemy)
+        {
+            base.Attack(enemy);
+            enemy.HP += 2;
+        }
+    }
+}
