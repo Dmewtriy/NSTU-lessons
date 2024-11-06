eb94f653c1aefbefd810180ee63f0a60fba76084
Tair <gidrolaz.tr@gmail.com>
2024-11-06 11:26:46 +0700

    Класс зелья

--- lab3/lab3/Spell.cs
+++ lab3/lab3/Spell.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,53 @@ namespace lab3
 {
     internal abstract class Spell
     {
-        private string name;
-        private string description;
+        private string name; // название зелья
+        private string description; // краткое описание
+        private int price; // стоимость карты(монеты)
+        public string Name
+        {
+            get 
+            { 
+                return name; 
+            }
+            set 
+            {
+                if (!string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value))
+                {
+                    name = value;
+                }
+                else name = "Spell";
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value))
+                {
+                    description = value;
+                }
+                else description = "None";
+            }
+        }
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value > 0 && value < 5)
+                {
+                    price = value;
+                }
+                else price = 1;
+            }
+        }
     }
 }
