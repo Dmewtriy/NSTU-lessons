99f2e1264a905a3c9c051d92845b8c271793d35f
Dmewtriy <dankin.dm@mail.ru>
2024-11-07 22:08:23 +0700

    Добавлена возможность сохранять и открывать мобов в формате json
    Метод SaveToJson() -> Сохраняет объект класса в директорию карт
    Статический метод LoadFromJson() -> Возвращает список мобов, хранящихся в директории

--- lab3/lab3/Mob.cs
+++ lab3/lab3/Mob.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace lab3
@@ -12,7 +14,9 @@ namespace lab3
         private int damage;
         private int price;
         private string name;
-        private string history;
+        private string history; // Женин таск
+        public string Type => GetType().Name; // Свойство Type для указания типа
+        public static readonly string path = "..\\..\\..\\cards";
 
         public int HP
         {
@@ -64,5 +68,55 @@ namespace lab3
 
         public virtual void Attack(Fly enemy)
         {}
+
+        public void SaveToJson()
+        {
+            string fileName = $"{name}.json";
+            string filePath = path + "\\" + fileName;
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(this, options);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static List<Mob> LoadFromJson()
+        {
+            string[] cardFiles = Directory.GetFiles(path);
+            var mobs = new List<Mob>();
+            string jsonData, typeName;
+            JsonElement element;
+            foreach (string cardFile in cardFiles)
+            {
+                jsonData = File.ReadAllText(cardFile);
+                element = JsonSerializer.Deserialize<JsonElement>(jsonData);
+                typeName = element.GetProperty("Type").GetString();
+
+                Mob mob;
+
+                switch (typeName)
+                {
+                    case "Archer":
+                        mob = JsonSerializer.Deserialize<Archer>(jsonData);
+                        break;
+                    case "Fly":
+                        mob = JsonSerializer.Deserialize<Fly>(jsonData);
+                        break;
+                    case "Mage":
+                        mob = JsonSerializer.Deserialize<Mage>(jsonData);
+                        break;
+                    case "Melee":
+                        mob = JsonSerializer.Deserialize<Melee>(jsonData);
+                        break;
+                    case "Tank":
+                        mob = JsonSerializer.Deserialize<Tank>(jsonData);
+                        break;
+                    default:
+                        mob = JsonSerializer.Deserialize<Mob>(jsonData);
+                        break;
+                }
+
+                mobs.Add(mob);
+            }
+            return mobs;
+        }
     }
 }
