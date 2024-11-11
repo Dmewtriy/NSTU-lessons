f3e6bd6693e2fbd3eab563e942b2c7e087ccdddf
Dmewtriy <dankin.dm@mail.ru>
2024-11-11 17:53:27 +0700

    Удалены лишние юзинги во всех файлах

--- lab3/lab3/Spell.cs
+++ lab3/lab3/Spell.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace lab3
 {
