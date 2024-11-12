683bb9ab705e343af37eccb12d7b385cac457f4c
Dmewtriy <dankin.dm@mail.ru>
2024-11-12 20:08:45 +0700

    Создание класса AllCards

---
+++ lab3/lab3/AllCards.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace lab3
+{
+    internal class AllCards
+    {
+        public List<Mob> Mobs { get; }
+        public List<Spell> Spells { get; }
+
+        public AllCards()
+        {
+            MobPersistence mobPersistence = new MobPersistence();
+            SpellPersistence spellPersistence = new SpellPersistence();
+            Mobs = mobPersistence.LoadFromJson();
+            Spells = spellPersistence.LoadFromJson();
+        }
+    }
+}
