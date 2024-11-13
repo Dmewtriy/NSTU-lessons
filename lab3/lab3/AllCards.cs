93b214d2858eb72b2b1752b8dc89a57df3af8b0d
Dmewtriy <dankin.dm@mail.ru>
2024-11-13 15:02:30 +0700

    Доработка связанная с колодой
    Класс AllCards теперь хранит List<Card>
    Создан класс DeckPrint для печати колоды

--- lab3/lab3/AllCards.cs
+++ lab3/lab3/AllCards.cs
@@ -4,15 +4,12 @@ namespace lab3
 {
     internal class AllCards
     {
-        public List<Mob> Mobs { get; }
-        public List<Spell> Spells { get; }
+        public List<Card> Cards { get; }
 
         public AllCards()
         {
-            MobPersistence mobPersistence = new MobPersistence();
-            SpellPersistence spellPersistence = new SpellPersistence();
-            Mobs = mobPersistence.LoadFromJson();
-            Spells = spellPersistence.LoadFromJson();
+            CardPersistence cardPersistence = new CardPersistence();
+            Cards = cardPersistence.LoadFromJson();
         }
     }
 }
---
+++ lab3/lab3/DeckPrint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal static class DeckPrint
+    {
+        public static void Print(List<Card> deck)
+        {
+            foreach (Card card in deck) 
+            { 
+                Console.WriteLine(card);
+            }
+        }
+    }
+}
