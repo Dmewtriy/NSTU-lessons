6b549fb4cdb36d9b7e88043e7e3e396bcaea11b5
Dmewtriy <dankin.dm@mail.ru>
2024-11-14 16:26:53 +0700

    Добавлены интерфейс и классы состояния игры

---
+++ lab3/lab3/WaitingForOpponentAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab3
+{
+    internal class WaitingForOpponentAction : Interfaces.IGameStatus
+    {
+        public void Action(Card attacker, Mob defender)
+        {
+            throw new Exception("Вы не можете атаковать. Дождитесь конца хода соперника");
+        }
+
+        public void EnterStatus()
+        {
+            throw new Exception("Ход соперника");
+        }
+
+        public void Waiting()
+        {
+            throw new Exception("Ожидание хода соперника...");
+        }
+    }
+}
