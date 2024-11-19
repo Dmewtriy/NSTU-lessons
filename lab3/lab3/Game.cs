8c34972ee3579b357c4daa03a3fa3594f855670a
Dmewtriy <dankin.dm@mail.ru>
2024-11-19 23:17:26 +0700

    Добавлен класс игра

---
+++ lab3/lab3/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Game
+    {
+        private Interfaces.IGameStatus currentStatus;
+        public Interfaces.IGameStatus CurrentStatus
+        {
+            get
+            {
+                return currentStatus;
+            }
+            set
+            {
+                if (value == null) { throw new ArgumentNullException("Текущее состояние игры не может быть null"); }
+                currentStatus = value;
+            }
+        }
+        private Player player1;
+        private Player player2;
+        public Player Player1
+        {
+            get { return player1; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Player1 cannot be null.");
+                player1 = value;
+            }
+        }
+
+        public Player Player2
+        {
+            get { return player2; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Player2 cannot be null.");
+                player2 = value;
+            }
+        }
+    }
+}
