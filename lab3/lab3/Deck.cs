f1fc88bb848bf3e0936f21d5cd98c13ddc03512b
Tair <gidrolaz.tr@gmail.com>
2024-12-04 15:23:32 +0700

    Перегрузил AddCard тепрь стало проще.

--- lab3/lab3/Deck.cs
+++ lab3/lab3/Deck.cs
@@ -25,9 +25,23 @@ namespace lab3
                 throw new ArgumentException("Колода уже полная");
 
             cards.Add(card);
-
         }
+        public void AddCard(Card card1, Card card2)
+        {
+            if (card1 == null || card2 == null)
+                throw new ArgumentException("Неверная карта");
+            if (cards.Contains(card1))
+                throw new ArgumentException($"Карта {card1.Name} уже в колоде. Выберите другую карту.");
+            if (cards.Contains(card2))
+                throw new ArgumentException($"Карта {card2.Name} уже в колоде. Выберите другую карту.");
 
+            if (RemoveCard(card1)) 
+            {
+                throw new ArgumentException($"Метод удаления карты не сработал");
+            }
+            AddCard(card2);
+            
+        }
         public bool RemoveCard(Card card)
         {
             return cards.Remove(card);
