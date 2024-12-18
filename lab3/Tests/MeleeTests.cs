343075c2c2ea4515f95993203b51771c882e406a
Tair <gidrolaz.tr@gmail.com>
2024-12-18 23:12:15 +0700

    Доделал модульные тесты и изменил логику класса LowAttackSpell: убрал продолжительность эффекта

--- lab3/Tests/MeleeTests.cs
+++ lab3/Tests/MeleeTests.cs
@@ -15,70 +15,70 @@ namespace MobTest
         public void Test_Melee_Action_InteractionWithArcher()
         {
             // Arrange
-            var melee = new Melee { Hp = 20, Damage = 4 };
-            var archer = new Archer { Hp = 15, Damage = 6 };
+            var melee = new Melee { Hp = 10, Damage = 4 };
+            var archer = new Archer { Hp = 7, Damage = 6 };
 
             // Act
             archer.Action(melee);
 
             // Assert
-            Assert.AreEqual(14, melee.Hp);
+            Assert.AreEqual(4, melee.Hp);
         }
 
         [TestMethod]
         public void Test_Melee_Action_InteractionWithFly()
         {
             // Arrange
-            var melee = new Melee { Hp = 20, Damage = 3 };
-            var fly = new Fly { Hp = 10, Damage = 2 };
+            var melee = new Melee { Hp = 10, Damage = 3 };
+            var fly = new Fly { Hp = 5, Damage = 2 };
 
             // Act
             fly.Action(melee);
 
             // Assert
-            Assert.AreEqual(18, melee.Hp); 
+            Assert.AreEqual(8, melee.Hp); 
         }
 
         [TestMethod]
         public void Test_Melee_Action_InteractionWithMage()
         {
             // Arrange
-            var melee = new Melee { Hp = 25, Damage = 4 };
-            var mage = new Mage { Hp = 12, Damage = 7 };
+            var melee = new Melee { Hp = 10, Damage = 4 };
+            var mage = new Mage { Hp = 3, Damage = 7 };
 
             // Act
             mage.Action(melee);
 
             // Assert
-            Assert.AreEqual(18, melee.Hp);
+            Assert.AreEqual(3, melee.Hp);
         }
 
         [TestMethod]
         public void Test_Melee_Action_InteractionWithTank()
         {
             // Arrange
-            var melee = new Melee { Hp = 30, Damage = 6 };
+            var melee = new Melee { Hp = 10, Damage = 6 };
             var tank = new Tank { Hp = 10, Damage = 8 };
 
             // Act
             tank.Action(melee);
 
             // Assert
-            Assert.AreEqual(22, melee.Hp); 
+            Assert.AreEqual(2, melee.Hp); 
         }
 
         [TestMethod]
         public void Test_Melee_Action_InteractionWithMelee()
         {
             // Arrange
-            var melee1 = new Melee { Hp = 25, Damage = 5 };
-            var melee2 = new Melee { Hp = 20, Damage = 4 };
+            var melee1 = new Melee { Hp = 10, Damage = 5 };
+            var melee2 = new Melee { Hp = 8, Damage = 4 };
 
             // Act
             melee1.Action(melee2);
 
             // Assert
-            Assert.AreEqual(15, melee2.Hp); 
+            Assert.AreEqual(3, melee2.Hp); 
         }
     }
 }
--- lab3/Tests/TankTests.cs
+++ lab3/Tests/TankTests.cs
@@ -15,70 +15,70 @@ namespace MobTest
         public void Test_Tank_Action_InteractionWithArcher()
         {
             // Arrange
-            var tank = new Tank { Hp = 20, Damage = 4 };
-            var archer = new Archer { Hp = 15, Damage = 6 };
+            var tank = new Tank { Hp = 10, Damage = 4 };
+            var archer = new Archer { Hp = 3, Damage = 6 };
 
             // Act
             archer.Action(tank);
 
             // Assert
-            Assert.AreEqual(16, tank.Hp); // Archer наносит урон Tank с уменьшением на 2, итоговый урон 4
+            Assert.AreEqual(6, tank.Hp); // Archer наносит урон Tank с уменьшением на 2, итоговый урон 6
         }
 
         [TestMethod]
         public void Test_Tank_Action_InteractionWithFly()
         {
             // Arrange
-            var tank = new Tank { Hp = 20, Damage = 3 };
+            var tank = new Tank { Hp = 10, Damage = 3 };
             var fly = new Fly { Hp = 10, Damage = 2 };
 
             // Act
             fly.Action(tank);
 
             // Assert
-            Assert.AreEqual(18, tank.Hp); // Fly наносит Tank полный урон (2)
+            Assert.AreEqual(8, tank.Hp); // Fly наносит Tank полный урон (2)
         }
 
         [TestMethod]
         public void Test_Tank_Action_InteractionWithMage()
         {
             // Arrange
-            var tank = new Tank { Hp = 25, Damage = 4 };
-            var mage = new Mage { Hp = 12, Damage = 7 };
+            var tank = new Tank { Hp = 10, Damage = 4 };
+            var mage = new Mage { Hp = 4, Damage = 7 };
 
             // Act
             mage.Action(tank);
 
             // Assert
-            Assert.AreEqual(18, tank.Hp); // Mage наносит Tank полный урон (7)
+            Assert.AreEqual(3, tank.Hp); // Mage наносит Tank полный урон (7)
         }
 
         [TestMethod]
         public void Test_Tank_Action_InteractionWithMelee()
         {
             // Arrange
-            var tank = new Tank { Hp = 30, Damage = 6 };
+            var tank = new Tank { Hp = 10, Damage = 6 };
             var melee = new Melee { Hp = 10, Damage = 8 };
 
             // Act
             melee.Action(tank);
 
             // Assert
-            Assert.AreEqual(24, tank.Hp); // Melee наносит Tank урон с уменьшением на 2, итоговый урон 6
+            Assert.AreEqual(4, tank.Hp); // Melee наносит Tank урон с уменьшением на 2, итоговый урон 6
         }
 
         [TestMethod]
         public void Test_Tank_Action_InteractionWithTank()
         {
             // Arrange
-            var tank1 = new Tank { Hp = 25, Damage = 5 };
-            var tank2 = new Tank { Hp = 20, Damage = 4 };
+            var tank1 = new Tank { Hp = 10, Damage = 5 };
+            var tank2 = new Tank { Hp = 8, Damage = 4 };
 
             // Act
             tank1.Action(tank2);
 
             // Assert
-            Assert.AreEqual(15, tank2.Hp); // Tank наносит другому Tank полный урон (5)
+            Assert.AreEqual(3, tank2.Hp); // Tank наносит другому Tank полный урон (5)
         }
     }
 }
