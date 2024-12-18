082de87f041bb4bf1e4ab841f782f9c1dab6c2dc
Tair <gidrolaz.tr@gmail.com>
2024-12-18 11:47:33 +0700

    Json файла для карт

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.CodeDom.Compiler;
+using System.Collections.Generic;
 
 namespace lab3
 {
@@ -7,19 +8,7 @@ namespace lab3
     {
         static void Main()
         {
-            AllCards allCards = new AllCards();
-            Deck deck1 = new Deck();
-            Deck deck2 = new Deck();
-            foreach (Card card in allCards.Cards) 
-            { 
-                deck1.AddCard(card.Clone() as Card);
-            }
-            Player player1 = new Player(deck1);
-            Player player2 = new Player(deck2);
-            Settings settings = new Settings();
-            Game game = new Game(player1, player2, settings);
-            player1.CardsInHand.Remove(player1.Deck.Cards[0]);
-            int a = 5;
         }
     }
 }
+
